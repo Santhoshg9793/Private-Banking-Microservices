fcbe46e1e7b1d99f32cc40660706d88e2c124d3d
Santhosh Gnanewaran <santhoshg9793@gmail.com>
2020-09-05 12:43:03 +0530

    Added API gateway Service
    Added Customer Profile Information Database System Zuul Gateway service

---
+++ cupid-gateway-service/src/test/java/com/cupid/system/CupidGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupid.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
