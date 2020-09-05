cd2668aef42782f314ab8ccf9a95ba76c0c86332
Santhosh Gnanewaran <santhoshg9793@gmail.com>
2020-09-05 12:41:46 +0530

    Added Registry Server
    Added Customer Profile Information Database System Eureka Server

---
+++ cupid-registry-server/src/test/java/com/cupid/system/CupidRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupid.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
